c79040630b26eaf764c8bc65528c12015a4946e3
antsiborav <AnciborAV@mail.ru>
2024-06-30 17:15:20 +0300

    2 internationalization + HATEOAS

--- src/main/java/com/aav/micro/controller/LicenseController.java
+++ src/main/java/com/aav/micro/controller/LicenseController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping(value = "gr/organization/{organizationId}/license")
 public class LicenseController {
@@ -13,11 +18,18 @@ public class LicenseController {
 		@Autowired
 		private LicenseService licenseService;
 
-		@GetMapping(value = "/{licenseId}")
-		public ResponseEntity<License> getLicense(
-						@PathVariable("organizationId") String organizationId,
-						@PathVariable("licenseId") String licensedId) {
-				License license = licenseService.getLicense(licensedId, organizationId);
+		@RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
+		public ResponseEntity<License> getLicense( @PathVariable("organizationId") String organizationId,
+		                                           @PathVariable("licenseId") String licenseId) {
+
+				License license = licenseService.getLicense(licenseId, organizationId);
+				license.add(
+								linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
+								linkTo(methodOn(LicenseController.class).createLicense(organizationId, license, null)).withRel("createLicense"),
+								linkTo(methodOn(LicenseController.class).updateLicense(organizationId, license)).withRel("updateLicense"),
+								linkTo(methodOn(LicenseController.class).deleteLicense(organizationId, license.getLicenseId())).withRel("deleteLicense")
+				);
+
 				return ResponseEntity.ok(license);
 		}
 
@@ -32,8 +44,9 @@ public class LicenseController {
 		@PostMapping
 		public ResponseEntity<String> createLicense(
 						@PathVariable("organizationId") String organizationId,
-						@RequestBody License request) {
-				return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+						@RequestBody License request,
+						@RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+				return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
 		}
 
 		@DeleteMapping(value = "/{licenseId}")
--- src/main/java/com/aav/micro/service/LicenseService.java
+++ src/main/java/com/aav/micro/service/LicenseService.java
@@ -1,14 +1,21 @@
 package com.aav.micro.service;
 
 import com.aav.micro.model.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.Random;
 
 @Service
 public class LicenseService {
 
-		public License getLicense(String licenseId, String organizationId) {
+		@Autowired
+		MessageSource messageSource;
+
+		public License getLicense(String licenseId,
+		                          String organizationId) {
 				License license = new License();
 				license.setId(new Random().nextInt(1000));
 				license.setLicenseId(licenseId);
@@ -19,11 +26,15 @@ public class LicenseService {
 				return license;
 		}
 
-		public String createLicense(License license, String organizationId) {
+		public String createLicense(License license,
+		                            String organizationId,
+		                            Locale locale) {
 				String responseMessage = null;
 				if (license != null) {
 						license.setOrganizationId(organizationId);
-						responseMessage = String.format("Successfully created a new license with ID: %s", license.toString());
+						responseMessage =
+										String.format(messageSource.getMessage("license.create.message",
+														null, locale), license.toString());
 				}
 				return responseMessage;
 		}
@@ -32,14 +43,18 @@ public class LicenseService {
 				String responseMessage = null;
 				if (license != null) {
 						license.setOrganizationId(organizationId);
-						responseMessage = String.format("Successfully updated a license with ID: %s", license.toString());
+						responseMessage =
+										String.format(messageSource.getMessage("licence.update.message",
+														null, null), license.toString());
 				}
 				return responseMessage;
 		}
 
 		public String deleteLicense(String licenseId, String organizationId) {
 				String responseMessage = null;
-				responseMessage = String.format("Successfully deleted a license with ID: %s", licenseId);
+				responseMessage =
+								String.format(messageSource.getMessage("licence.delete.message",
+												null, null), licenseId, organizationId);
 				return responseMessage;
 		}
 }
